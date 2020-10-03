3422f077fd5c10da778a3de7835bad0d6b21a1d2
Shaga Sresthaa <shagasresthaa@gmail.com>
2020-10-03 11:43:20 +0530

    added code for a basic node to detect temp,press,etc working on esp8266 later

--- arduinoCode/arduinoNanoNodes/arduinoNanoNodes.ino
+++ arduinoCode/arduinoNanoNodes/arduinoNanoNodes.ino
@@ -1,9 +1,69 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <Wire.h>
+#include <Adafruit_BMP085.h>
 
+#include <SPI.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 32 // OLED display height, in pixels
+
+// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+#define OLED_RESET     4 // Reset pin # (or -1 if sharing Arduino reset pin)
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+Adafruit_BMP085 bmp;
+
+void setup() {
+  Serial.begin(9600);
+  if (!bmp.begin()) {
+  Serial.println("Could not find a valid BMP085 sensor, check wiring!");
+  while (1) {}
+  }
+
+  // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3C for 128x32
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;); // Don't proceed, loop forever
+  }
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  float temp,pres,alti;
+  temp = bmp.readTemperature();
+  pres = bmp.readPressure()/1000;
+  alti = bmp.readAltitude(101500);
+  
+  display.clearDisplay();
+  display.setTextSize(1);      // Normal 1:1 pixel scale
+  display.setTextColor(SSD1306_WHITE); // Draw white text
+  display.setCursor(0, 0);     // Start at top-left corner
+  display.println("");
+  display.print("Temp = ");
+  display.print(temp);
+  display.println(" C");
+  display.print("Pres = ");
+  display.print(pres);
+  display.println(" KPa");
+  display.print("Alti = ");
+  display.print(alti);
+  display.println(" meters");
+  display.display();
+  delay(2000);
 
+    
+  Serial.print("Temperature = ");
+  Serial.print(bmp.readTemperature());
+  Serial.println(" *C");
+
+  Serial.print("Pressure = ");
+  Serial.print(bmp.readPressure());
+  Serial.println(" Pa");
+
+  Serial.print("Real altitude = ");
+  Serial.print(bmp.readAltitude(101500));
+  Serial.println(" meters");
+    
+  Serial.println();
+  delay(500);
 }
