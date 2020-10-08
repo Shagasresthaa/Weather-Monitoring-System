8f17917b9d3c8e3c8473408b73e36ddf7e046ffd
Shaga Sresthaa <shagasresthaa@gmail.com>
2020-10-08 09:36:18 +0530

    added support for dht11 sensor

--- arduinoCode/arduinoNanoNodes/arduinoNanoNodes.ino
+++ arduinoCode/arduinoNanoNodes/arduinoNanoNodes.ino
@@ -8,6 +8,11 @@
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 32 // OLED display height, in pixels
 
+//DHT11 config
+#include <SimpleDHT.h>
+#define pinDHT11 8
+SimpleDHT11 dht11(pinDHT11);
+
 // Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
 #define OLED_RESET     4 // Reset pin # (or -1 if sharing Arduino reset pin)
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
@@ -29,16 +34,25 @@ void setup() {
 }
 
 void loop() {
-  float temp,pres,alti;
+
+  byte temperature = 0;
+  byte humidity = 0;
+  int err = SimpleDHTErrSuccess;
+  if ((err = dht11.read(&temperature, &humidity, NULL)) != SimpleDHTErrSuccess) {
+    Serial.print("Read DHT11 failed, err="); Serial.println(err);delay(1000);
+    return;
+  }
+  
+  float temp,pres,alti,humd;
   temp = bmp.readTemperature();
   pres = bmp.readPressure()/1000;
   alti = bmp.readAltitude(101500);
+  humd = float(humidity);
   
   display.clearDisplay();
   display.setTextSize(1);      // Normal 1:1 pixel scale
   display.setTextColor(SSD1306_WHITE); // Draw white text
   display.setCursor(0, 0);     // Start at top-left corner
-  display.println("");
   display.print("Temp = ");
   display.print(temp);
   display.println(" C");
@@ -48,20 +62,27 @@ void loop() {
   display.print("Alti = ");
   display.print(alti);
   display.println(" meters");
+  display.print("Humd = ");
+  display.print(humd);
+  display.println(" g/m-3");
   display.display();
   delay(2000);
 
     
   Serial.print("Temperature = ");
-  Serial.print(bmp.readTemperature());
+  Serial.print(temp);
   Serial.println(" *C");
 
   Serial.print("Pressure = ");
-  Serial.print(bmp.readPressure());
-  Serial.println(" Pa");
+  Serial.print(pres);
+  Serial.println(" KPa");
 
   Serial.print("Real altitude = ");
-  Serial.print(bmp.readAltitude(101500));
+  Serial.print(alti);
+  Serial.println(" meters");
+
+  Serial.print("Humidity = ");
+  Serial.println(humd);
   Serial.println(" meters");
     
   Serial.println();
