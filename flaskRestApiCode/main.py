169aa77eca3452f3d7164fcf6d48076147de0d6f
Shaga Sresthaa <shagasresthaa@gmail.com>
2020-10-16 14:50:57 +0530

    added sqlalchemy database code

--- flaskRestApiCode/main.py
+++ flaskRestApiCode/main.py
@@ -1,8 +1,21 @@
 from flask import Flask
 from flask_restful import Api,Resource
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 api = Api(app)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database/weather.db'
+db = SQLAlchemy(app)
+
+class NodeListAndStatus(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    status = db.Column(db.String(30), nullable = False)
+
+    def __repr__(self):
+        return f"wstat(id={id},status={status})"
+
+db.create_all()
 
 class HelloWorld(Resource):
     def get(self):
